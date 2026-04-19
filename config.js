993db2678ccea4ad4aa8f3c2a4957cf174e0b7d6
Suji2812 <sujithabodicherla28@gmail.com>
2026-04-19 17:52:52 +0530

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-const GEMINI_API_KEY = 'AIzaSyAiZCWhXVNhmMUGQhiDgRre1F5TDABPubg';
-const GEMINI_URL = "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=" + GEMINI_API_KEY;
